f4c7adfcf2b0dd2a72e02dc97bdb340b36943196
semenovoleg98 <semenovoleg988@gmail.com>
2020-04-24 15:49:17 +0300

    initial commit

---
+++ scripts/fade-slider.js
@@ -0,0 +1,16 @@
+(function(){
+
+    const slides = document.querySelectorAll('.fade-slider__item');
+    let index = 0;
+    const activeClass = "fade-slider__item--visible";
+ 
+    setInterval(function(){
+        slides[index].classList.remove(activeClass);
+        index++;
+        if(index + 1 > slides.length){
+            index = 0;
+        }
+        slides[index].classList.add(activeClass);
+    }, 5000);
+
+}())
